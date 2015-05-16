010349cb01d158ec195c4860556b91e11fb0794f
yuki maeta <pattern.ao@gmail.com>
2015-05-16 16:43:51 +0900

    Scene遷移後に選択人数が取得できるようにする

--- Assets/Scripts/NumberSelectScene.cs
+++ Assets/Scripts/NumberSelectScene.cs
@@ -7,16 +7,23 @@ using System.Collections;
 
 public class NumberSelectScene : MonoBehaviour {
 	
-	[SerializeField] private Button p2Btn;
-	[SerializeField] private Button p3Btn;
-	[SerializeField] private Button p4Btn;
-	
+	[SerializeField] private Button P2Btn;
+	[SerializeField] private Button P3Btn;
+	[SerializeField] private Button P4Btn;
+
+	public static int selectedNumber = 2;  // Default 2 people
+
+	public virtual void Awake() {
+
+		DontDestroyOnLoad(this);
+	}
+
 	// Use this for initialization
 	void Start () {
 
-		p2Btn.onClick.AsObservable().Subscribe(_ =>selectedButton(p2Btn));
-		p3Btn.onClick.AsObservable().Subscribe(_ =>selectedButton(p3Btn));
-		p4Btn.onClick.AsObservable().Subscribe(_ =>selectedButton(p4Btn));
+		P2Btn.onClick.AsObservable().Subscribe(_ =>selectedButton(P2Btn));
+		P3Btn.onClick.AsObservable().Subscribe(_ =>selectedButton(P3Btn));
+		P4Btn.onClick.AsObservable().Subscribe(_ =>selectedButton(P4Btn));
 	}
 	
 	// Update is called once per frame
@@ -26,14 +33,17 @@ public class NumberSelectScene : MonoBehaviour {
 
 	void selectedButton( Button btn ) {
 
-		if ( btn.Equals(this.p2Btn) ) {
-			Debug.Log("SELECTED : 2" );
+		if ( btn.Equals(this.P2Btn) ) {
+
+			NumberSelectScene.selectedNumber = 2;
 		}
-		else if ( btn.Equals(this.p3Btn) ) {
-			Debug.Log("SELECTED : 3" );
+		else if ( btn.Equals(this.P3Btn) ) {
+
+			NumberSelectScene.selectedNumber = 3;
 		}
-		else if ( btn.Equals(this.p4Btn) ) {
-			Debug.Log("SELECTED : 4" );
+		else if ( btn.Equals(this.P4Btn) ) {
+
+			NumberSelectScene.selectedNumber = 4;
 		}
 	}
 }
