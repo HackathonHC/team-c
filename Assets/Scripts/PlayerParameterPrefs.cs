5a63d6e6ab7c7f8284c68b765fcd703cb48af00b
早田大輔 <dai0217@gmail>
2015-05-16 15:26:31 +0900

    bf

--- Assets/Scripts/PlayerParameterPrefs.cs
+++ Assets/Scripts/PlayerParameterPrefs.cs
@@ -5,48 +5,48 @@ public class PlayerParameterPrefs
 {
 	private int Id;
 
-	PlayerParameterPrefs (int id)
+	public PlayerParameterPrefs (int id)
 	{
 		Id = id;
 	}
 
 	public void SetX (int x)
 	{
-		PlayerPrefs.SetInt (Id.ToString + "_X", x);
+		PlayerPrefs.SetInt (Id.ToString() + "_X", x);
 	}
 
 	public void GetX ()
 	{
-		PlayerPrefs.GetInt (Id.ToString + "_X");
+		PlayerPrefs.GetInt (Id.ToString() + "_X");
 	}
 
-	public void GetY (int x)
+	public void SetY (int y)
 	{
-		PlayerPrefs.SetInt (Id.ToString + "_Y", x);
+		PlayerPrefs.SetInt (Id.ToString() + "_Y", y);
 	}
 	
-	public void SetY ()
+	public void GetY ()
 	{
-		PlayerPrefs.GetInt (Id.ToString + "_Y");
+		PlayerPrefs.GetInt (Id.ToString() + "_Y");
 	}
 	
 	public void SetCharacterTypeId (int typeid) 
 	{	
-		PlayerPrefs.SetInt (Id.ToString + "_CharacterType", typeid);
+		PlayerPrefs.SetInt (Id.ToString() + "_CharacterType", typeid);
 	}
 
 	public void GetCharacterTypeId () 
 	{	
-		PlayerPrefs.GetInt (Id.ToString + "_CharacterType");
+		PlayerPrefs.GetInt (Id.ToString() + "_CharacterType");
 	}
 	
-	public void SetDeadFlg ()
+	public void SetDeadFlg (int deadFlg)
 	{
-		PlayerPrefs.SetInt (Id.ToString + "_DeadFlg", 1);
+		PlayerPrefs.SetInt (Id.ToString() + "_DeadFlg", deadFlg);
 	}
 
 	public void GetDeadFlg ()
 	{
-		PlayerPrefs.GetInt (Id.ToString + "_DeadFlg");
+		PlayerPrefs.GetInt (Id.ToString() + "_DeadFlg");
 	}
 }
