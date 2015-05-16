4b50c4fb7d352a92964129079c27c896d0a22d0f
早田大輔 <dai0217@gmail>
2015-05-16 15:12:40 +0900

    Playerデータクラス追加 & キャラクタークラス追加

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player
+{
+	private int Id { get; set; }
+	private int X { get; set; }
+	private int Y { get; set; }
+	private int CharacterTypeId { get; set; }
+	private int DeadFlg { get; set; }
+	private PlayerParameterPrefs Param { get; set; }
+
+	Player (int id) 
+	{
+		Id = id;
+		Param = new PlayerParameterPrefs(Id);
+		X = 0;
+		Y = 1;
+		CharacterTypeId = 1;
+		DeadFlg = 0;
+	}
+
+	void Save () 
+	{
+		Param.SetX(X);
+		Param.SetY(Y);
+		Param.SetCharacterTypeId(CharacterTypeId);
+		Param.SetDeadFlg(DeadFlg);
+	}
+
+}
---
+++ Assets/Scripts/PlayerParameterPrefs.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerParameterPrefs 
+{
+	private int Id;
+
+	PlayerParameterPrefs (int id)
+	{
+		Id = id;
+	}
+
+	public void SetX (int x)
+	{
+		PlayerPrefs.SetInt (Id.ToString + "_X", x);
+	}
+
+	public void GetX ()
+	{
+		PlayerPrefs.GetInt (Id.ToString + "_X");
+	}
+
+	public void GetY (int x)
+	{
+		PlayerPrefs.SetInt (Id.ToString + "_Y", x);
+	}
+	
+	public void SetY ()
+	{
+		PlayerPrefs.GetInt (Id.ToString + "_Y");
+	}
+	
+	public void SetCharacterTypeId (int typeid) 
+	{	
+		PlayerPrefs.SetInt (Id.ToString + "_CharacterType", typeid);
+	}
+
+	public void GetCharacterTypeId () 
+	{	
+		PlayerPrefs.GetInt (Id.ToString + "_CharacterType");
+	}
+	
+	public void SetDeadFlg ()
+	{
+		PlayerPrefs.SetInt (Id.ToString + "_DeadFlg", 1);
+	}
+
+	public void GetDeadFlg ()
+	{
+		PlayerPrefs.GetInt (Id.ToString + "_DeadFlg");
+	}
+}
