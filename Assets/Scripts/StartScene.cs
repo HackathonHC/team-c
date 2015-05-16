f18c0dbea7c84da485798167b4da7574efca6881
早田大輔 <dai0217@gmail>
2015-05-16 14:17:08 +0900

    sample

--- Assets/Scripts/StartScene.cs
+++ Assets/Scripts/StartScene.cs
@@ -1,15 +1,27 @@
 ﻿using UnityEngine;
+using UniRx;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 using System.Collections;
 
 public class StartScene : MonoBehaviour {
 
+	[SerializeField]
+	private Button startBtn;
+
 	// Use this for initialization
 	void Start () {
-	
+		startBtn.onClick.AsObservable().Subscribe(_ =>GameStart());
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	void GameStart () {
+		Application.LoadLevel("NumberSelectScene");
 	}
 }
