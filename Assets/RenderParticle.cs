4e9c4d0f17493c2584f6bc72004bf3b06049dfe0
早田大輔 <dai0217@gmail>
2015-05-17 14:22:18 +0900

    レイヤー追加

---
+++ Assets/RenderParticle.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RenderParticle : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Renderer>().sortingLayerName = "background";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
